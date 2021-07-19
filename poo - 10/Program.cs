abc8789f0ce588046b9564bc35ddd45504e55b2f
joaoros <joaodaros.contato@gmail.com>
2021-07-19 11:07:25 -0300

    starting to code multidimensional-array exercise

---
+++ poo - 10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace poo___10 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual o tamanho da matriz? ");
+            int n = int.Parse(Console.Readline());
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i<n; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j<n; j++) {
+                    mat[i,j] = int.Parse(valores[j]);
+                }
+            }
+        }
+    }
+}
