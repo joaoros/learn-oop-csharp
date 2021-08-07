ba2741392f533f68a150e701cf32e6a25bdb4615
joaoros <joaodaros.contato@gmail.com>
2021-08-06 23:41:35 -0300

    adding 12th exercise

---
+++ 12th exercise/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace _12th_exercise.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
--- 12th exercise/Program.cs
+++ 12th exercise/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using _12th_exercise.Entities;
+using _12th_exercise.Entities.Enums;
 
-namespace _12th_exercise 
-{
-    class Program 
-    {
+namespace _12th_exercise {
+    class Program  {
+        static void Main(string[] args) {
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
 
-        static void Main(string[] args) 
-        {
-
-            Console.WriteLine("Hello World!");
-            
+            System.Console.WriteLine(order);
         }
     }
 }
