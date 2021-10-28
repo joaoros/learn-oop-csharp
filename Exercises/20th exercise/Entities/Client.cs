037d6ce7655888f4352f4522303f84b785a31bf1
joaoros <joaodaros.contato@gmail.com>
2021-10-28 02:00:14 -0300

    adding some exercises

---
+++ Exercises/20th exercise/Entities/Client.cs
@@ -0,0 +1,19 @@
+namespace _20th_exercise.Entities {
+    public class Client {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Client)){
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Exercises/20th exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using _20th_exercise.Entities;
+
+namespace _20th_exercise {
+    class Program {
+        static void Main(string[] args) {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "João", Email = "joao@gmail.com"};
+
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
